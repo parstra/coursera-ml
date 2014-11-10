d10fdabe54de893bdb547df39db1e343ee02fa14
Stratos Paraskevaidis <stratosiii@gmail.com>
2014-11-11 00:36:28 +0200

    Exercise 5.1 done

--- mlclass-ex5-007/mlclass-ex5/linearRegCostFunction.m
+++ mlclass-ex5-007/mlclass-ex5/linearRegCostFunction.m
@@ -19,16 +19,8 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-
-
-
-
-
-
-
-
-
-
+J += sum((X * theta - y) .** 2) + lambda * sum(theta(2:end, :) .** 2);
+J /= 2*m
 
 % =========================================================================
 
