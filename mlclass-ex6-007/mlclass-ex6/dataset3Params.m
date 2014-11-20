0868c717b23108cf5c14e55e6cdcdff3a616e330
Stratos Paraskevaidis <stratosiii@gmail.com>
2014-11-20 23:56:11 +0200

    Exercise 6.2 done

--- mlclass-ex6-007/mlclass-ex6/dataset3Params.m
+++ mlclass-ex6-007/mlclass-ex6/dataset3Params.m
@@ -23,11 +23,26 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]';
+sigma_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]';
+m = size(C_vec)(1);
+err = 10000;
 
+for i=1:m
+  C_temp = C_vec(i);
+  for j=1:m
+    sigma_temp = sigma_vec(j);
+    model = svmTrain(X, y, C_temp, @(x1, x2) gaussianKernel(x1, x2, sigma_temp), 1e-3, 5);
+    predictions = svmPredict(model, Xval);
+    new_err = mean(double(predictions ~= yval));
 
-
-
-
+    if new_err < err
+      C = C_temp;
+      sigma = sigma_temp;
+      err = new_err;
+    end
+  end
+end
 
 % =========================================================================
 
